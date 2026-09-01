1edc7d54b5cd57ff756de533188cda6db526c286
Joshika <joshikareddy2k4@gmail.com>
2026-09-01 14:48:26 +0530

    Add schema scripts and deploy workflow

---
+++ schema/001_create_database.sql
@@ -0,0 +1,4 @@
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'MyAppDB')
+BEGIN
+    CREATE DATABASE MyAppDB;
+END
---
+++ schema/002_create_tables.sql
@@ -0,0 +1,11 @@
+USE MyAppDB;
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Users')
+BEGIN
+    CREATE TABLE Users (
+        UserId INT IDENTITY(1,1) PRIMARY KEY,
+        Name NVARCHAR(100) NOT NULL,
+        Email NVARCHAR(255) NOT NULL,
+        CreatedAt DATETIME DEFAULT GETDATE()
+    );
+END
